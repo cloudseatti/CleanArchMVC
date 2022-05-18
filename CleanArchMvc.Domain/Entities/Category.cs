80fdc6a02f692ea860c41567ba77dc57e34bf795
cloudseatti <102782945+cloudseatti@users.noreply.github.com>
2022-05-18 01:05:48 -0300

    Enriquecendo o Dominio Category Implementando os construtores e regras de negocio

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,12 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchMvc.Domain.Validation;
+using System.Collections.Generic;
 
 namespace CleanArchMvc.Domain.Entities
 {
     public class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
+            Id = id;
+            ValidationDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidationDomain(name);
+        }
 
         public ICollection<Product> Products { get; set; }
+
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+                "Invalid name. Name is required");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name. Too Short, minimum 3 characters");
+
+            Name = name;
+        }
+
     }
 }
