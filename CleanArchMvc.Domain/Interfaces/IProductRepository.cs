e0159f9766890d643e55881796a629525cc072d2
cloudseatti <102782945+cloudseatti@users.noreply.github.com>
2022-05-18 09:16:36 -0300

    Criando interface das classes product e category

---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductyAsync();
+        Task<Product> GetBtyIdAsync(int? id);
+
+        Task<Product> GetProductCateboryAsync(int? id);
+
+        Task<Product> UpdateAsync(Product category);
+        Task<Product> RemoveAsync(Product category);
+        Task<Product> CreateAsync(Product category);
+
+    }
+}
