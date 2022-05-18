53855ea1fb853274601ef02cebf97d27f47f0964
cloudseatti <102782945+cloudseatti@users.noreply.github.com>
2022-05-18 00:45:48 -0300

    Adicionar arquivos de projeto.

---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+
+        public int Categoryid { get; set; }
+        public Category Category { get; set; }
+    }
+}
